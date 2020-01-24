fec19d0ee2734d7e82f4ee860edb7525e7f03123
Richard Gross <richard.gross@maibornwolff.de>
2020-01-24 21:16:58 +0100

    adds class generator

---
+++ annotations/src/main/kotlin/de/richargh/sandbox/kaptcodegen/annnotations/GenName.kt
@@ -0,0 +1,3 @@
+package de.richargh.sandbox.kaptcodegen.annnotations
+
+annotation class GenName
--- app/src/main/kotlin/de/richargh/sandbox/kaptcodegen/app/Main.kt
+++ app/src/main/kotlin/de/richargh/sandbox/kaptcodegen/app/Main.kt
@@ -1,5 +1,11 @@
 package de.richargh.sandbox.kaptcodegen.app
 
+import de.richargh.sandbox.kaptcodegen.annnotations.GenName
+
 fun main(args: Array<String>) {
+    println("Hello ${Generated_Hello().getName()}")
     println(Car().greet())
 }
+
+@GenName
+class Hello
