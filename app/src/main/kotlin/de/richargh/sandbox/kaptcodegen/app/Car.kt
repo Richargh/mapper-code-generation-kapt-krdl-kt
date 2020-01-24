32624830ff48a76638ca11918eeba71978c89c2c
Richard Gross <richard.gross@maibornwolff.de>
2020-01-24 20:16:23 +0100

    found a simple class with annotation processing

---
+++ app/src/main/kotlin/de/richargh/sandbox/kaptcodegen/app/Car.kt
@@ -0,0 +1,7 @@
+package de.richargh.sandbox.kaptcodegen.app
+
+import de.richargh.sandbox.kaptcodegen.annnotations.Interesting
+
+@Interesting
+class Car {
+}
--- app/src/main/kotlin/de/richargh/sandbox/kaptcodegen/app/main.kt
+++ app/src/main/kotlin/de/richargh/sandbox/kaptcodegen/app/main.kt
@@ -1,8 +1,5 @@
 package de.richargh.sandbox.kaptcodegen.app
 
-import de.richargh.sandbox.kaptcodegen.annnotations.Interesting
-
-@Interesting
 fun main(args: Array<String>) {
     println("The compiler should have told you I am interesting")
 }
