c51b5c67d9dd0d599e9d285f51db03f21765664c
Richard Gross <richard.gross@maibornwolff.de>
2020-01-28 08:01:14 +0100

    print all constructor parameters and their parameters

--- app/src/main/kotlin/de/richargh/sandbox/kaptcodegen/app/Main.kt
+++ app/src/main/kotlin/de/richargh/sandbox/kaptcodegen/app/Main.kt
@@ -3,9 +3,11 @@ package de.richargh.sandbox.kaptcodegen.app
 import de.richargh.sandbox.kaptcodegen.annnotations.Mapper
 
 fun main(args: Array<String>) {
-    println("Hello ${HelloMapper().map(Hello("none"))}")
+    println("Hello ${HelloMapper().map(Hello("none", 42, Person("John")))}")
     println(Car().greet())
 }
 
 @Mapper
-data class Hello(val name: String)
+data class Hello(val name: String, val messageCount: Int, val person: Person)
+
+data class Person(val name: String)
--- processor/src/main/kotlin/de/richargh/sandbox/kaptcodegen/processor/MapperGenerator.kt
+++ processor/src/main/kotlin/de/richargh/sandbox/kaptcodegen/processor/MapperGenerator.kt
@@ -8,6 +8,8 @@ import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.TypeElement
 
 @AutoService(Processor::class)
@@ -22,14 +24,43 @@ class MapperGenerator: AbstractProcessor() {
     }
 
     override fun process(set: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
-        roundEnv.getElementsAnnotatedWith(Mapper::class.java).forEach {
-            val className = it.simpleName.toString()
+        roundEnv.getElementsAnnotatedWith(Mapper::class.java).forEach { element ->
+            val className = element.simpleName.toString()
             println("Processing: $className")
-            val pack = processingEnv.elementUtils.getPackageOf(it).toString()
+            val pack = processingEnv.elementUtils.getPackageOf(element).toString()
             generateClass(className, pack)
+
+            element.enclosedElements.forEach { enclosed ->
+                when(enclosed.kind) {
+                    ElementKind.CONSTRUCTOR -> {
+                        val constructor = enclosed as ExecutableElement
+                        println("$enclosed ${enclosed.kind} in ${enclosed.enclosedElements}")
+
+                        constructor.parameters.forEach { parameter ->
+                            val type = parameter.asType()
+
+                            val parameterElement = processingEnv.typeUtils.asElement(type)
+                            when(parameterElement){
+                                is TypeElement -> {
+                                    println("Constructor argument: ${parameter.simpleName} : $type")
+                                    parameterElement.enclosedElements.forEach {
+                                        if(it is ExecutableElement && it.kind == ElementKind.CONSTRUCTOR){
+                                            println("-- ${it.parameters}")
+                                            // processingEnv.typeUtils.getPrimitiveType()
+                                        }
+                                    }
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+            }
         }
         return true
     }
+
 
     private fun generateClass(className: String, pack: String) {
         val fileName = "${className}Mapper"
@@ -37,7 +68,7 @@ class MapperGenerator: AbstractProcessor() {
                 .addType(TypeSpec.classBuilder(fileName)
                                  .addFunction(FunSpec.builder("map")
                                                       .addParameter(className, ClassName(pack, className))
-                                                      .addStatement("""return $className("bar")""")
+                                                      .addStatement("""return $className("bar", 21, Person("Me"))""")
                                                       .build())
                                  .build())
                 .build()
